4b12d98208752a89b786251bc7b5fae383c7ce20
gamahead <joshua.rose@rochester.edu>
2014-10-21 12:23:47 -0400

    Gotta stop dropping :mods table since it doesn't exist on heroku

--- db/migrate/20141017161948_create_mods.rb
+++ db/migrate/20141017161948_create_mods.rb
@@ -1,7 +1,6 @@
 # -*- encoding : utf-8 -*-
 class CreateMods < ActiveRecord::Migration
   def change
-    drop_table :mods
     create_table :mods do |t|
       t.string :aType
       t.string :vidOneSource
