f2b803441f7b6a5d94bd6c905e5a662f86fe7511
gamahead <gamahead@gmail.com>
2014-11-18 11:44:47 -0500

    still debugging production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,13 +12,6 @@
 //
 //
 //= require turbolinks
-//= require dropdown
-//= require modal
-//= require transition
-//= require tooltip
-//= require popover
-//= require button
-//= require bootstrap.min
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require bootstrap.min
