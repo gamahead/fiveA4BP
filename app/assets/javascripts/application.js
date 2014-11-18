724b9e9381427bb9fa04a7d22ec0b9aa04948b1f
gamahead <gamahead@gmail.com>
2014-11-18 11:49:16 -0500

    switching order of jquery and bootstrap in app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //
-//= require turbolinks
+//= require bootstrap.min
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap.min
+//= require turbolinks
