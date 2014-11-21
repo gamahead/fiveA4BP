04eee5d21588fba246dbaf54d33440835ec33ae6
gamahead <gamahead@gmail.com>
2014-11-21 12:15:46 -0500

    again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require_self
-//= require ttpop
 //= require jquery
 //= require jquery_ujs
+//= require ttpop
 //= require turbolinks
 //= require bootstrap
