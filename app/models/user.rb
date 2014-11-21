d960d4467838c496567c0fee2dca2ea6578d75f1
gamahead <gamahead@gmail.com>
2014-11-21 16:06:24 -0500

    Heroku breaks everything

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
 	validates :name,  presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
-  validates :clinic
+  validates :clinic, presence: true
   has_secure_password
   validates :password, length: { minimum: 6 }
 
