c3bb16d578b39e5ae94da29b4d8d340a03eab577
gamahead <gamahead@gmail.com>
2015-03-19 01:52:19 -0400

    maybe fixing something

--- app/mailers/doc_mailer.rb
+++ app/mailers/doc_mailer.rb
@@ -1,5 +1,5 @@
 class DocMailer < ActionMailer::Base
-  default from: ENV['gmail_username']
+  default from: :user_name
   def welcome_email(user)
   	@user = user
   	mail(to: @user.email, subject: 'This is a test')
