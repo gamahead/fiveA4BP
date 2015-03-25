7c5ef1790abf3446074033a7c1a6984c601aaf7f
Josh Rose <gamahead@gmail.com>
2015-03-25 09:36:21 -0400

    Fixed Videos

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,8 +35,8 @@ User.create!(
 Mod.create(
 
 aType: "Ask/Assess", 
-vidOneSource: "//www.youtube.com/embed/2Vb8dg_un-A", 
-vidTwoSource: "//www.youtube.com/embed/jKXvA_gq3Rc", 
+vidOneSource: "https://www.youtube.com/embed/0knXQOM0Tpw", 
+vidTwoSource: "https://www.youtube.com/embed/fXWsGQSFnkE", 
 explanation: "In the second video, the doctor asked two several important questions. First, he asked if there was something else. This helps get all the issues on the agenda. Second, he asked the patient for permission to address the blood pressure first. Last, he asked if she understood her goal.", 
 
 dialogueOne: 
@@ -68,8 +68,8 @@ dialogTwo:
 Mod.create(
 
 aType: "Advise", 
-vidOneSource: "//www.youtube.com/embed/Eub5UNfaaiU", 
-vidTwoSource: "//www.youtube.com/embed/qqUNyC660bc", 
+vidOneSource: "https://www.youtube.com/embed/qSfXClaYNgI", 
+vidTwoSource: "https://www.youtube.com/embed/1jtIk1vzf1E", 
 explanation: "In both videos, the doctor provides some useful advice. However, in the first one, the doctor is more interactive – drawing answers out of the patient rather than just giving out information. This approach analogous to motivational interviewing helps patients process the information better.", 
 
 dialogueOne: 
@@ -94,8 +94,8 @@ dialogTwo:
 Mod.create(
 
 aType: "Agree", 
-vidOneSource: "//www.youtube.com/embed/c3fDbcxCWUs", 
-vidTwoSource: "//www.youtube.com/embed/Far9qrxzdW0", 
+vidOneSource: "https://www.youtube.com/embed/GKcHDfZgtiE", 
+vidTwoSource: "https://www.youtube.com/embed/dEFIcBpI_Uc", 
 explanation: "In the first video, the doctor provides a lot of information and then asks the patient if that is OK. It is more helpful, to chunk the information and obtain agreement for each step. First the doctor asks Janice if she willing to commit to the goal. Next, the doctor raises the issue of meeting monthly until her blood pressure is controlled. This puts this issue out there for discussion and supports the patient’s autonomy in decision making.", 
 
 dialogueOne: 
@@ -121,8 +121,8 @@ dialogTwo:
 Mod.create(
 
 aType: "Assist", 
-vidOneSource: "//www.youtube.com/embed/B4klm_L3WvI", 
-vidTwoSource: "//www.youtube.com/embed/We1Tz6ZtNOw", 
+vidOneSource: "https://www.youtube.com/embed/UvAl_-qU7kI", 
+vidTwoSource: "https://www.youtube.com/embed/ugOCiZilNgk", 
 explanation: "In the first video, the doctor provides a lot of information and asks the patient at the end if that is OK. In the second video, the doctor explores some reasons why the patient might be having trouble with medication adherence. The same approach can be applied to lifestyle changes.", 
 
 dialogueOne: 
@@ -144,8 +144,8 @@ dialogTwo:
 Mod.create(
 
 aType: "Arrange", 
-vidOneSource: "//www.youtube.com/embed/ZJj6G6jUcxw", 
-vidTwoSource: "//www.youtube.com/embed/US8leQpN5vs" , 
+vidOneSource: "https://www.youtube.com/embed/tz90OewWN6g", 
+vidTwoSource: "https://www.youtube.com/embed/-YI3MIIfT1g" , 
 explanation: "In both videos, the doctor provides a summary for the patient at end. This can be very helpful in promoting recall. However, in the second video, the approach is more patient centered. Most importantly, the doctor confirms that the patient is still willing to commit to monthly visit (i.e. arranging follow-up).", 
 
 dialogueOne: 
