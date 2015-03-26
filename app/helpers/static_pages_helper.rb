85b24433fcc732469cc9f49fc55cf658a5a5ea2e
gamahead <gamahead@gmail.com>
2015-03-25 23:00:55 -0400

    Aesthetic improvements

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,3 +1,18 @@
 # -*- encoding : utf-8 -*-
 module StaticPagesHelper
+
+  def numeric_to_english(num)
+    case num
+    when "1"
+      "One"
+    when "2"
+      "Two"
+    when "3"
+      "Three"
+    when "4"
+      "Four"
+    when "5"
+      "Five"
+    end
+  end
 end
