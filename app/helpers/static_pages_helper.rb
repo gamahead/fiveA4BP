d367a77a6dbe5e723b85f92e456b2f29a27367ea
Josh Rose <gamahead@gmail.com>
2015-03-30 17:57:25 -0400

    Wrestling with aesthetics

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -18,10 +18,11 @@ module StaticPagesHelper
 
   # Outputs necessary popover html contingent on whether the module has been completed
   def popover_like_its_hot(mod_num)
-    if completed?(mod_num)
+    if completed?(mod_num-1)
       return ""
     else
-      return "data-toggle=\"popover\" data-trigger=\"hover\" data-placement=\"top\" title=\"Incomplete\" data-content=\"Module #{mod_num} has not yet been completed\""
+      return "data-toggle=\"popover\" data-trigger=\"hover\" data-placement=\"top\""\
+      "title=\"Incomplete\" data-content=\"Module #{mod_num} has not yet been completed\""
     end
   end
 end
